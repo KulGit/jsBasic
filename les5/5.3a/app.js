f77e50b31a2a5773ced438c9acadfb5967bfd9d1
KulGit <exciland@yandex.ru>
2020-08-26 18:16:09 +0300

    add 5.3

---
+++ les5/5.3a/app.js
@@ -0,0 +1,23 @@
+// 1. получить объект модального окна с классом .wrap
+const getObject = document.querySelector('.wrap');
+
+
+// 2. получить тег span, используемый для закрытия окна
+const closeWindow = document.querySelectorAll('span');
+
+// 3. получить кнопку, используемую для показа модального окна
+const buttonEnter = document.querySelector('button');
+
+
+
+// 4. назначить обработку клика на кнопку показа модального окна
+// когда функция обработчик срабатывает она должна у модального
+// окна удалять класс hidden
+buttonEnter.addEventListener('click', function() {
+    getObject.classList.remove('hidden')
+})
+
+
+// 5. назначить обработку клика на тег span, при клике
+// в обработчике модальному окну должен быть добавлен
+// класс hidden
