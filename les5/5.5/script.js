ba43e54a940061b9ecdea03a13a27853c361653e
pavelWork <exciland@yandex.ru>
2020-09-01 21:19:20 +0300

    make 5.5

--- les5/5.5/script.js
+++ les5/5.5/script.js
@@ -1,29 +1,10 @@
 'use strict';
 
-let chessBoard = document.getElementById("cell");
-
-for ( let i=0;i<8;i++) {
-    let row = chessBoard.appendChild(document.createElement("div"));
-    for (let j=0;j<8;j++) {
-        row.appendChild(document.createElement("span"));    }
-}
-
-
-
-// var x=8;
-// var y=8;
-
-// var chessBoard = document.getElementById("chessBoard");
-
-// for (var i=0; i<y; i++){
-//     var row = chessBoard.appendChild(document.createElement("div"));
-//     for (var j=0; j<x; j++){
-//         row.appendChild(document.createElement("span"));
-//     }
-// }
-
+let numbers = [1,2,3,4,5,6,7,8];
+let letters = ['A','B','C','D','E','G','H'];
 
 var table = document.createElement("table");
+
 for (var i = 1; i < 9; i++) {
        var tr = document.createElement('tr');
        for (var j = 1; j < 9; j++) {
@@ -38,6 +19,29 @@ for (var i = 1; i < 9; i++) {
        table.appendChild(tr);
 }
 document.body.appendChild(table);
+
+
+
+// let chessBoard = document.getElementById("cell");
+
+// for ( let i=0;i<8;i++) {
+//     let row = chessBoard.appendChild(document.createElement("div"));
+//     for (let j=0;j<8;j++) {
+//         row.appendChild(document.createElement("span"));    }
+// }
+
+
+// var x=8;
+// var y=8;
+
+// var chessBoard = document.getElementById("chessBoard");
+
+// for (var i=0; i<y; i++){
+//     var row = chessBoard.appendChild(document.createElement("div"));
+//     for (var j=0; j<x; j++){
+//         row.appendChild(document.createElement("span"));
+//     }
+// }
 
 
 
